55138f9e2883e1023d2ee48b639c42cd3680bf9b
satria78sh <56376986+satria78sh@users.noreply.github.com>
2021-09-25 14:35:46 +0700

    Initialize ardalis/projectref

---
+++ src/CleanArchitecture.Web/Startup.cs
@@ -0,0 +1,81 @@
+﻿using CleanArchitecture.Core.Interfaces;
+using CleanArchitecture.Infrastructure.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using StructureMap;
+using System;
+
+namespace CleanArchitecture.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            string dbName = Guid.NewGuid().ToString();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase(dbName));
+                //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc()
+                .AddControllersAsServices();
+
+            var container = new Container();
+
+            container.Configure(config =>
+            {
+                config.Scan(_ =>
+                {
+                    _.AssembliesAndExecutablesFromApplicationBaseDirectory();
+                    //_.AssemblyContainingType(typeof(Startup)); // Web
+                    //_.AssemblyContainingType(typeof(BaseEntity)); // Core
+                    //_.Assembly("CleanArchitecture.Infrastructure"); // Infrastructure
+                    _.WithDefaultConventions();
+                    _.ConnectImplementationsToTypesClosing(typeof(IHandle<>));
+                    _.LookForRegistries();
+                });
+
+
+                //Populate the container using the service collection
+                config.Populate(services);
+            });
+
+            return container.GetInstance<IServiceProvider>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
