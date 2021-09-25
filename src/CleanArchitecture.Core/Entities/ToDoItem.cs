2d2dd0524336994756265f724917225b1cee7ae7
satria78sh <56376986+satria78sh@users.noreply.github.com>
2021-09-25 14:35:47 +0700

    Initialize ardalis/customkeys

---
+++ src/CleanArchitecture.Core/Entities/ToDoItem.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchitecture.Core.Events;
+using CleanArchitecture.Core.SharedKernel;
+
+namespace CleanArchitecture.Core.Entities
+{
+    public class ToDoItem : BaseEntity<ToDoItemKey>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; private set; } = false;
+
+        public void MarkComplete()
+        {
+            IsDone = true;
+            Events.Add(new ToDoItemCompletedEvent(this));
+        }
+    }
+
+    public struct ToDoItemKey
+    {
+        private int _id;
+
+        public ToDoItemKey(int id)
+        {
+            _id = id;
+        }
+
+        public static implicit operator ToDoItemKey(int value)
+        {
+            return new ToDoItemKey(value);
+        }
+
+        public static implicit operator int(ToDoItemKey me)
+        {
+            return me._id;
+        }
+    }
+}
